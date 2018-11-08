08651a2b3e3c1ad0edfecb4d121aa9fb5775b68a
Mitchell Harris <mitchell.harris2@gmail.com>
2018-11-07 18:47:14 -0600

    Adding basic basic database, bible data, jpa entity, and h2 connection - Mitchell Harris

---
+++ src/main/kotlin/com/doesnotreturnempty/BibleApi/bibleverse/BibleVerse.kt
@@ -0,0 +1,32 @@
+package com.doesnotreturnempty.BibleApi.bibleverse
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+
+@Entity(name = "engwebp_vpl")
+data class BibleVerse(
+
+        @Id
+        @Column(name = "verseID")
+        val id: String,
+
+        @Column(nullable = false)
+        val canonOrder: String,
+
+        @Column(nullable = false)
+        val book: String,
+
+        @Column(nullable = false)
+        val chapter: String,
+
+        @Column(name="STARTVERSE", nullable = false)
+        val startVerse: String,
+
+        @Column(name="ENDVERSE", nullable = false)
+        val endVerse: String,
+
+        @Column(name="VERSETEXT", nullable = false, length=5120)
+        val verseText: String
+)
